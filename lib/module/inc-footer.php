3a1d31e0a5fc25f642b15323940b7be09b0973d9
GhostGame-GG <108606954+GhostGame-GG@users.noreply.github.com>
2022-07-03 13:33:42 +0600

    Add files via upload

---
+++ lib/module/inc-footer.php
@@ -0,0 +1,126 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-ico-logo darkmode-footer-logo">
+					</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="">
+									<div class="footer-tx1-bound">
+										<span>Ресурсы</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://discord.gg/azfDeEwvBs" target="_blank">
+										<span>● Discord</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/GhostCorporation" target="_blank">
+										<span>● VK-группа</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.youtube.com/channel/UCSBO8Z8V4GbF-1zcS9VLsNw" target="_blank">
+										<span>● YouTube</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://steamcommunity.com/groups/FamGhost" target="_blank">
+										<span>● Steam</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://socialclub.rockstargames.com/crew/devildeimos/wall" target="_blank">
+										<span>● Social Club</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="">
+									<div class="footer-tx1-bound">
+										<span>Контакты</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/ghost_0666" target="_blank">
+										<span>● VK</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td class="footer-con-sources">
+							<div class="footer-table-in">
+								<div class="">
+									<div class="footer-tx1-bound">
+										<span>Важно</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://forms.gle/reTEYu4UE8CtgXgx8" target="_blank">
+										<span>● Служба поддержки</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://docs.google.com/document/d/1nD2eShoGHlgiVQMgCJH5jQVzDLGSabXI9rbkwo-hdO4/edit?usp=sharing" target="_blank">
+										<span>● Пользовательское соглашение</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://docs.google.com/document/d/1KXeEqpLgoHKQb_6uYxvGc4zwxKvWzanS7D8ShTZUtNU/edit?usp=sharing" target="_blank">
+										<span>● Правила пользователей</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						<div class="footer-con-side">
+							<div class="footer-tx1-founder">
+								<div class="footer-ico-developer" style="background: url('/img/icons/footer/hykem.png') center center / cover no-repeat;">
+								</div>
+								<a href="/about">
+								<span>Rustam Ghost</span>
+								</a>
+							</div>
+						</div>
+						<div class="footer-con-side">
+							<div class="footer-tx1-webmaster">
+								<div class="footer-ico-developer" style="background: url('/img/users/daginatsuko.png') center / cover no-repeat;">
+								</div>
+								<a href="" target="_blank">
+								<span>technicalsection.com@gmail.com</span>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
